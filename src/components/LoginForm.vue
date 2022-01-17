d3afdbce9c4742da72a3a83c7a2d8a803a80fd61
rndtim <51507837+rndtim@users.noreply.github.com>
2022-01-17 21:19:26 +0100

    Start the project. Basic structure

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+export default {
+
+  name: 'LoginForm',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/UserList.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+export default {
+
+  name: 'UserList',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
